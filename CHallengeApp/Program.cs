a91d992afa780b139aa16e7c48965b82ab9cd2f1
AGNIESZKAJA <agnieszka.jastrzewska@gmail.com>
2023-02-05 23:39:42 +0100

    Zadanie domowe dzień 4

--- CHallengeApp/Program.cs
+++ CHallengeApp/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello Aga");
+﻿string name = "Agnieszka";
+bool woman = true;
+int age = 43;
+
+if (woman = true && age < 30)
+
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Agnieszka" && age == 43)
+{
+    Console.WriteLine("Agnieszka, lat 43");
+}
+else if (woman = false && age < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+
+
+
+
+
+
+
+
